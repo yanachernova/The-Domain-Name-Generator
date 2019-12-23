c904160328531d8286a3bf065d537fe6a81a8aa4
yanachernova <58434635+yanachernova@users.noreply.github.com>
2019-12-23 17:28:11 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,20 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  let divYana = document.querySelector("#yana");
+  for (let i = 0; i < pronoun.length; i++) {
+    //console.log(pronoun[i]);
+    for (let j = 0; j < adj.length; j++) {
+      //console.log(adj[j]);
+      for (let k = 0; k < noun.length; k++) {
+        //console.log(noun[k]);
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        divYana.append(pronoun[i] + adj[j] + noun[k] + ".com ");
+      }
+    }
+  }
 };
